456c30923ba90043f797305cce89c4e5182a6f58
smugge <smugge@227660be-8329-0410-b27f-27df3c269e39>
2007-04-28 09:39:59 +0000

    added new MiG source

---
+++ trunk/src/main/java/MiG/oneclick/FileException.java
@@ -0,0 +1,10 @@
+package MiG.oneclick;
+
+public class FileException extends MiG.oneclick.Exception
+{
+   // Constructor
+   public FileException(String message)
+     {
+	super(message);
+     }
+}
